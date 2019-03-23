c6d7d9eb5efdab4e001889a2e6209dcda1810943
Paulo Clavijo Esteban <paucls@gmail.com>
2019-03-23 11:21:51 +0000

    Running test 100 times to have stronger golden copy

--- java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
@@ -1,7 +1,6 @@
 package com.adaptionsoft.games.trivia;
 
 import com.adaptionsoft.games.trivia.runner.GameRunner;
-import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,134 +28,18 @@ public class GoldenMasterTest {
         System.setOut(fileStream);
 
         // Act
-        gameRunner.main(new String[]{}, 123);
+        for (int i = 0; i < 100; i++) {
+            gameRunner.main(new String[]{}, i);
+        }
 
         // Assert
-        final FileInputStream fileInputStream = new FileInputStream("output.txt");
-        final String output = readOutputFile(fileInputStream);
-
-        assertEquals("Chet was added\n" +
-                        "They are player number 1\n" +
-                        "Pat was added\n" +
-                        "They are player number 2\n" +
-                        "Sue was added\n" +
-                        "They are player number 3\n" +
-                        "Chet is the current player\n" +
-                        "They have rolled a 3\n" +
-                        "Chet's new location is 3\n" +
-                        "The category is Rock\n" +
-                        "Rock Question 0\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Chet now has 1 Gold Coins.\n" +
-                        "Pat is the current player\n" +
-                        "They have rolled a 2\n" +
-                        "Pat's new location is 2\n" +
-                        "The category is Sports\n" +
-                        "Sports Question 0\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Pat now has 1 Gold Coins.\n" +
-                        "Sue is the current player\n" +
-                        "They have rolled a 1\n" +
-                        "Sue's new location is 1\n" +
-                        "The category is Science\n" +
-                        "Science Question 0\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Sue now has 1 Gold Coins.\n" +
-                        "Chet is the current player\n" +
-                        "They have rolled a 5\n" +
-                        "Chet's new location is 8\n" +
-                        "The category is Pop\n" +
-                        "Pop Question 0\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Chet now has 2 Gold Coins.\n" +
-                        "Pat is the current player\n" +
-                        "They have rolled a 1\n" +
-                        "Pat's new location is 3\n" +
-                        "The category is Rock\n" +
-                        "Rock Question 1\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Pat now has 2 Gold Coins.\n" +
-                        "Sue is the current player\n" +
-                        "They have rolled a 5\n" +
-                        "Sue's new location is 6\n" +
-                        "The category is Sports\n" +
-                        "Sports Question 1\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Sue now has 2 Gold Coins.\n" +
-                        "Chet is the current player\n" +
-                        "They have rolled a 3\n" +
-                        "Chet's new location is 11\n" +
-                        "The category is Rock\n" +
-                        "Rock Question 2\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Chet now has 3 Gold Coins.\n" +
-                        "Pat is the current player\n" +
-                        "They have rolled a 3\n" +
-                        "Pat's new location is 6\n" +
-                        "The category is Sports\n" +
-                        "Sports Question 2\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Pat now has 3 Gold Coins.\n" +
-                        "Sue is the current player\n" +
-                        "They have rolled a 1\n" +
-                        "Sue's new location is 7\n" +
-                        "The category is Rock\n" +
-                        "Rock Question 3\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Sue now has 3 Gold Coins.\n" +
-                        "Chet is the current player\n" +
-                        "They have rolled a 5\n" +
-                        "Chet's new location is 4\n" +
-                        "The category is Pop\n" +
-                        "Pop Question 1\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Chet now has 4 Gold Coins.\n" +
-                        "Pat is the current player\n" +
-                        "They have rolled a 3\n" +
-                        "Pat's new location is 9\n" +
-                        "The category is Science\n" +
-                        "Science Question 1\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Pat now has 4 Gold Coins.\n" +
-                        "Sue is the current player\n" +
-                        "They have rolled a 2\n" +
-                        "Sue's new location is 9\n" +
-                        "The category is Science\n" +
-                        "Science Question 2\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Sue now has 4 Gold Coins.\n" +
-                        "Chet is the current player\n" +
-                        "They have rolled a 4\n" +
-                        "Chet's new location is 8\n" +
-                        "The category is Pop\n" +
-                        "Pop Question 2\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Chet now has 5 Gold Coins.\n" +
-                        "Pat is the current player\n" +
-                        "They have rolled a 4\n" +
-                        "Pat's new location is 1\n" +
-                        "The category is Science\n" +
-                        "Science Question 3\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Pat now has 5 Gold Coins.\n" +
-                        "Sue is the current player\n" +
-                        "They have rolled a 2\n" +
-                        "Sue's new location is 11\n" +
-                        "The category is Rock\n" +
-                        "Rock Question 4\n" +
-                        "Question was incorrectly answered\n" +
-                        "Sue was sent to the penalty box\n" +
-                        "Chet is the current player\n" +
-                        "They have rolled a 2\n" +
-                        "Chet's new location is 10\n" +
-                        "The category is Sports\n" +
-                        "Sports Question 3\n" +
-                        "Answer was corrent!!!!\n" +
-                        "Chet now has 6 Gold Coins.\n",
-                output);
+        final String output = readOutputFile("output.txt");
+        final String expectedOutput = readOutputFile("expectedOutput.txt");
+        assertEquals(expectedOutput, output);
     }
 
-    private String readOutputFile(FileInputStream fileInputStream) throws IOException {
+    private String readOutputFile(String fileName) throws IOException {
+        final FileInputStream fileInputStream = new FileInputStream(fileName);
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
         String line = bufferedReader.readLine();
         final StringBuilder stringBuilder = new StringBuilder();
