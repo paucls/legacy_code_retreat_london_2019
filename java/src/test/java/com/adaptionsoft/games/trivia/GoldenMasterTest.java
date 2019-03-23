21f4338828e492b5bb4129c25fe98c2278456146
Paulo Clavijo Esteban <paucls@gmail.com>
2019-03-23 10:58:29 +0000

    Refactor test

--- java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
@@ -1,12 +1,11 @@
 package com.adaptionsoft.games.trivia;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -14,30 +13,29 @@ import java.io.PrintStream;
 import static junit.framework.Assert.assertEquals;
 
 public class GoldenMasterTest {
+
+    private Game aGame;
+
+    @Before
+    public void setUp() {
+        aGame = new Game();
+    }
+
     @Test
     public void a_winner_game() throws IOException {
-        String output = "";
-        String expectedOutput = "";
-        Game aGame = new Game();
+        // Arrange
         PrintStream fileStream = new PrintStream("output.txt");
         System.setOut(fileStream);
 
+        // Act
         aGame.add("Chet");
         aGame.add("Pat");
         aGame.add("Sue");
-
         aGame.roll(6);
 
         // Assert
         final FileInputStream fileInputStream = new FileInputStream("output.txt");
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
-        String line = bufferedReader.readLine();
-        final StringBuilder stringBuilder = new StringBuilder();
-
-        while (line != null) {
-            stringBuilder.append(line).append("\n");
-            line = bufferedReader.readLine();
-        }
+        final String output = readOutputFile(fileInputStream);
 
         assertEquals("Chet was added\n" +
                         "They are player number 1\n" +
@@ -50,6 +48,18 @@ public class GoldenMasterTest {
                         "Chet's new location is 6\n" +
                         "The category is Sports\n" +
                         "Sports Question 0\n",
-                stringBuilder.toString());
+                output);
+    }
+
+    private String readOutputFile(FileInputStream fileInputStream) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+        String line = bufferedReader.readLine();
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        while (line != null) {
+            stringBuilder.append(line).append("\n");
+            line = bufferedReader.readLine();
+        }
+        return stringBuilder.toString();
     }
 }
