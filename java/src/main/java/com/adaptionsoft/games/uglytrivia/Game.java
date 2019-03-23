0cee443c0360737be96ccdc7dd67e068f4e44957
Paulo Esteban <Paulo.Esteban@emc.com>
2019-03-23 12:35:42 +0000

    Remove unnecessary return type

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -34,9 +34,7 @@ public class Game {
 		return (howManyPlayers() >= 2);
 	}
 
-	public boolean add(String playerName) {
-
-
+	public void add(String playerName) {
 	    players.add(playerName);
 	    places[howManyPlayers()] = 0;
 	    purses[howManyPlayers()] = 0;
@@ -44,7 +42,6 @@ public class Game {
 
 	    System.out.println(playerName + " was added");
 	    System.out.println("They are player number " + players.size());
-		return true;
 	}
 
 	public int howManyPlayers() {
